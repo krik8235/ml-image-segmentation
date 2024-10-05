426a6d35b984d10109ac07c9019e417caf6b5522
krik8235 <krik8235@gmail.com>
2024-10-05 20:34:02 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Build an Image Segmentation Model for Medical Diagnosis using CNN, PyTorch and OpenCV
+
+In healthcare and medical science, the fusion of artificial intelligence and deep learning is revolutionizing diagnostics. My project focuses on the precise segmentation of polyps from colonoscopy images—a vital tool for medical practitioners.
+
+In this project, we aim to develop an image segmentation model using Pytorch.
+
+
+## Data
+
+The use of the CVC-Clinic database, containing frames from colonoscopy videos. The dataset includes polyp frames and corresponding ground truth images in both PNG and TIFF formats.
+
+
+## Tech Stack
+- **Deep Learning**: PyTorch
+- **Computer Vision**: OpenCV
+- **Libraries**: Scikit-learn, Pandas, NumPy, Albumentations
+
+
+
+## Takeaways
+
+- Polyp Segmentation Insights
+- IOU Metric Understanding
+- Data Augmentation Techniques
+- Practical PyTorch Data Augmentation
+- Medical Computer Vision Applications
+- Building CNN Models
---
+++ ml_deploy/train.py
@@ -0,0 +1,50 @@
+import torch
+from collections import OrderedDict
+from tqdm import tqdm
+from utils import AverageMeter, iou_score
+
+
+def train(deep_sup, train_loader, model, criterion, optimizer):
+    avg_meters = {'loss': AverageMeter(), 'iou': AverageMeter()}
+    model.train()
+    
+    pbar = tqdm(total=len(train_loader))
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    if input and target:
+        for input, target, _ in train_loader:
+            input = input.to(device)
+            target = target.to(device)
+
+            # compute output
+            if deep_sup:
+                outputs = model(input)
+                loss = 0
+                for output in outputs:
+                    loss += criterion(output, target)
+                loss /= len(outputs)
+                iou = iou_score(outputs[-1], target)
+            else:
+                output = model(input)
+                loss = criterion(output, target)
+                iou = iou_score(output, target)
+
+            # compute gradient and do optimizing step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            avg_meters['loss'].update(loss.item(), input.size(0))
+            avg_meters['iou'].update(iou, input.size(0))
+
+            postfix = OrderedDict([
+                ('loss', avg_meters['loss'].avg),
+                ('iou', avg_meters['iou'].avg),
+            ])
+            pbar.set_postfix(postfix)
+            pbar.update(1)
+
+    pbar.close()
+
+    return OrderedDict([('loss', avg_meters['loss'].avg),
+                        ('iou', avg_meters['iou'].avg)])
